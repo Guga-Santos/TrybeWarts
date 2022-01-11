ac69bc286ef2defdc7ab334b3ac6f78b2d85a0f6
GugaSantos <guga.santos.guga@gmail.com>
2022-01-11 15:39:25 -0300

    requisito 3

---
+++ script.js
@@ -0,0 +1,11 @@
+const loginBtn = document.querySelector('#login-btn');
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+
+loginBtn.addEventListener('click', () => {
+    if(email.value === 'tryber@teste.com' && senha.value === '123456') {
+        alert('Olá, Tryber!')
+    } else {
+        alert('Email ou senha inválidos.')
+    }
+})
