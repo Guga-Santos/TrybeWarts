9533c05f9eb2458e1964f556d335c7b608825d21
GugaSantos <guga.santos.guga@gmail.com>
2022-01-11 17:51:12 -0300

    Requisito 21 quase la

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ loginBtn.addEventListener('click', () => {
   }
 });
 
+// -----------------------------------------------------------
+
 const agreement = document.querySelector('#agreement');
 const submit = document.querySelector('#submit-btn');
 
@@ -22,3 +24,43 @@ function checked() {
 }
 
 agreement.addEventListener('click', checked)
+
+
+// ---------------------------------------------------------------
+
+const counter = document.querySelector('#counter');
+const texto = document.querySelector('#textarea');
+
+document.querySelector('#textarea').textLength
+document.querySelector('#textarea').maxLength
+
+function contador(e) {
+  const textLength = e.target.maxLength - e.target.textLength
+  counter.innerHTML = textLength
+}
+
+texto.addEventListener('keyup', contador)
+texto.addEventListener('keydown', contador)
+
+// -----------------------------------------------------------------
+
+const nome = document.querySelector('#input-name');
+const sobrenome = document.querySelector('#input-lastname');
+const emailForms = document.querySelector('#input-email');
+const casas = document.querySelector('#house');
+const familia = document.querySelector('#label-family');
+const materias = document.querySelector('#label-content');
+const avaliacao = document.querySelector('#label-rate');
+const observacoes = document.querySelector('#textarea');
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault()
+ document.querySelector('#evaluation-form').innerHTML = ''
+ document.querySelector('#evaluation-form').innerHTML = `<p> Nome: ${nome.value} ${sobrenome.value} </p>`
+ document.querySelector('#evaluation-form').innerHTML += `<p> Email: ${emailForms.value}</p>`
+ document.querySelector('#evaluation-form').innerHTML += `<p> Casa: ${casas.value} </p>`
+ document.querySelector('#evaluation-form').innerHTML += `<p> Família: ${familia.value} </p>`
+ document.querySelector('#evaluation-form').innerHTML += `<p> Matérias: ${materias.value} </p>`
+ document.querySelector('#evaluation-form').innerHTML += `<p> Avaliação: ${avaliacao.value} </p>`
+ document.querySelector('#evaluation-form').innerHTML += `<p> Observações: ${observacoes.value} </p>`
+})
